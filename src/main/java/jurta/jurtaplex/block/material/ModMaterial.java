dff77e9aabd6bb2dbec8fb0d95d4cc372a6aeb9f
JurtaHQ <29545237+JurtaHQ@users.noreply.github.com>
2020-11-14 20:52:52 +0000

    custom materials

---
+++ src/main/java/jurta/jurtaplex/block/material/ModMaterial.java
@@ -0,0 +1,10 @@
+package jurta.jurtaplex.block.material;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+
+public class ModMaterial {
+	public static final Material BASANITE = (new Material.Builder(MaterialColor.BLACK)).build();
+	public static final Material VANADINITE = (new Material.Builder(MaterialColor.ADOBE)).build();
+	public static final Material ZOISITE = (new Material.Builder(MaterialColor.YELLOW)).build();
+}
--- src/main/java/jurta/jurtaplex/init/ModBlocks.java
+++ src/main/java/jurta/jurtaplex/init/ModBlocks.java
@@ -3,10 +3,10 @@ package jurta.jurtaplex.init;
 import java.util.function.Supplier;
 
 import jurta.jurtaplex.block.ModOreBlock;
+import jurta.jurtaplex.block.material.ModMaterial;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
@@ -14,15 +14,15 @@ import net.minecraftforge.fml.RegistryObject;
 
 public class ModBlocks {
 	public static final RegistryObject<Block> BASANITE = register("basanite", () ->
-			new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5f, 6).harvestTool(ToolType.PICKAXE).harvestLevel(0).sound(SoundType.STONE).setRequiresTool()));
+			new Block(AbstractBlock.Properties.create(ModMaterial.BASANITE).hardnessAndResistance(1.5f, 6).harvestTool(ToolType.PICKAXE).harvestLevel(0).sound(SoundType.STONE).setRequiresTool()));
 	public static final RegistryObject<Block> ALTERNIS_VANADINITE_ORE = register("alternis_vanadinite_ore", () ->
-			new ModOreBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(3, 3).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.STONE).setRequiresTool()));
+			new ModOreBlock(AbstractBlock.Properties.create(ModMaterial.BASANITE).hardnessAndResistance(3, 3).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.STONE).setRequiresTool()));
 	public static final RegistryObject<Block> VANADINITE_BLOCK = register("vanadinite_block", () ->
-			new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(5, 6).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).setRequiresTool()));
+			new Block(AbstractBlock.Properties.create(ModMaterial.VANADINITE).hardnessAndResistance(5, 6).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).setRequiresTool()));
 	public static final RegistryObject<Block> ALTERNIS_ZOISITE_ORE = register("alternis_zoisite_ore", () ->
-			new ModOreBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(3, 3).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.STONE).setRequiresTool()));
+			new ModOreBlock(AbstractBlock.Properties.create(ModMaterial.BASANITE).hardnessAndResistance(3, 3).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.STONE).setRequiresTool()));
 	public static final RegistryObject<Block> ZOISITE_BLOCK = register("zoisite_block", () ->
-			new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(5, 6).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).setRequiresTool()));
+			new Block(AbstractBlock.Properties.create(ModMaterial.ZOISITE).hardnessAndResistance(5, 6).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).setRequiresTool()));
 	
 	static void register() {}
 	
